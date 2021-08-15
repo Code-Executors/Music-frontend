ab093a71b642361f9684d9340ae5ea65aa9af833
yazeed alshami <alshamiyazeed@gmail.com>
2021-08-15 17:20:29 +0300

    add footer

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import App from './App'
 import Header from './components/Header'
-
+import Footer from './components/Footer'
 export class Main extends Component {
     render() {
         return (
            <>
            <Header />
             <App />
+            <Footer/>
             </>
         )
     }
