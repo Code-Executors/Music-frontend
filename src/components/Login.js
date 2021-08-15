1bf7305c18431970252d2b5b4377102d70ad5fc1
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-08-15 15:23:46 +0300

    adding some features by bashars money

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container } from "react-bootstrap"
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=e1fe8085e345416ea70a5881724413af&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=29100d22a56f419e846f66a430615533&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 
 export default function Login() {
