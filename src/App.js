4e506143d746df7934ee5b516141e33db24de98f
b-aq <bashar.3akleh_92@hotmail.com>
2021-08-15 23:28:37 +0300

    added a backgound img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import './App.css'
 
 
 import React, { Component } from 'react'
@@ -11,9 +12,13 @@ const code = new URLSearchParams(window.location.search).get("code");
 export class App extends Component {
   render() {
     return (
-     
-      code ? <Dashboard code={code} /> : <Login />
+     <>
+     <img className="img" src="https://i.pinimg.com/originals/e6/0e/53/e60e531bb26f15c5f69c2cb35633bf46.jpg" alt=""/>
+    
+      {code ? <Dashboard code={code} /> : <Login />
       
+      }
+      </>
       )
     }
   }
