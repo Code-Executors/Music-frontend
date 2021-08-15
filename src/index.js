c30fa1eaf815d52dedf9866ded44c64d3fa16b9b
mdharb <md20.harb21@gmail.com>
2021-08-15 14:05:02 +0300

    modifying components, add Nav

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainApp from './MainApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <MainApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
