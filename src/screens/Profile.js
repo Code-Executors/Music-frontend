e2b5f041990cce6c68fec73e42dc93963d6a5330
b-aq <bashar.3akleh_92@hotmail.com>
2021-08-16 23:20:18 +0300

    added profile

---
+++ src/screens/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+export class Profile extends Component {
+    render() {
+        return (
+            <div>
+            <img src='' alt='' />
+            <p>Hello </p>
+            
+    
+          </div>
+        )
+    }
+}
+
+export default Profile
+
